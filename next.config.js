a39056bcd8519683586f8f4c4d1d4363d9c27b64
VicZambom <VictoriaG.ZambomP@gmail.com>
2025-08-27 18:32:16 -0300

    Configura o basePath para o GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  /* config options here */
+  basePath: '/Movieapp', 
+  images: {
+    unoptimized: true, 
+  },
 };
 
 module.exports = nextConfig;
